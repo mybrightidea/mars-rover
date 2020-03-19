385d8848da48667a69dc7bbf84d13992f104af28
John Lupton <johnlupton@JohnniMusicMac.local>
2020-03-19 22:40:56 +0000

    interim checkpoint

--- readme.md
+++ readme.md
@@ -2,6 +2,37 @@
 
 ## Solution Approach
 
+1. Data Preparation
+   The first line of data gives the maximum extent of the plateau whic simply needs to parsed
+
+We are then presented with rover movement data comprising starting coordinates, an orientation and then a sequence of rotations and translations.
+
+The starting orientation can transformed to be represented by rotations relative to the base orientation (North)
+
+So an orientation of:
+
+- East can be represented as North with 1 Right rotation applied
+- South as North with 2 Right rotations applied
+- West as North with either 1 Left or 3 Right rotations applied
+
+The movement data of each rover is a queue of instructions (`[R | L | M ]`).
+
+We can write an eqivalent representation of each rover's databy by PRE-pending its starting orientation represented by North with orienting rotations
+
+so Example 1
+
+- `{x:1, y:2, orientation: N, movement: L-M-L-M-L-M-L-M-M}` is unchanged
+
+and Example 2
+
+- `{x:3, y:3, orientation: E, movement: M-M-R-M-M-R-M-R-R-M}` is transformed to
+
+- `{x:3, y:3, orientation: N, movement: R-M-M-R-M-M-R-M-R-R-M}`
+
+Performing this pre-processing on the input data the means that subsequent processing logic is simplified and generic.
+
+2.
+
 Have Fun! :clown_face:
 
 John
--- script.js
+++ script.js
@@ -10,6 +10,30 @@ const grid = [
   [1, 1, 1, 0, 0]
 ];
 
+const input = [[5, 5], [1, 2, "N"], "LMLMLMLMM", [3, 3, "E"], "MMRMMRMRRM"];
+
+///////////////////////////////////////////////
+// Render result
+///////////////////////////////////////////////
+const renderResult = result => {
+  document.getElementById(
+    "result"
+  ).innerHTML = result.reduce((html, endPoint, roverNumber) => {}, "");
+};
+
+const renderInput = input => {
+  let html = "";
+
+  for (let roverNumber = 0; roverNumber < input.length; roverNumber++) {
+    const { x, y, orientation, data } = input[roverNumber];
+    html += `Rover: ${roverNumber +
+      1} start [${x},${y}] facing ${orientation} - data: ${data}<br/>`;
+  }
+
+  document.getElementById("input").innerHTML = html;
+};
+const runProblem = params => [];
+
 const gridInit = g => {
   const path = document.getElementById("rover-path");
   let html = "";
@@ -29,7 +53,50 @@ const gridInit = g => {
 // END OF PUZZLE RENDERING
 ///////////////////////////////////////////////
 
+// Parse input and run problem
+
+const orientationToRotation = orientation => {
+  switch (orientation) {
+    case "E":
+      return "R";
+    case "S":
+      return "RR";
+    case "W":
+      return "RRR";
+    default:
+      return "";
+  }
+};
+
+const prepData = input => {
+  const [maxX, maxY] = input[0];
+  let roverData = [];
+
+  for (let i = 1; i < input.length; i = i + 2) {
+    const [x, y, orientation] = input[i];
+    const data = input[i + 1].split("");
+    roverData.push({
+      x,
+      y,
+      orientation,
+      data,
+      adjustedData: orientationToRotation(orientation)
+        .split("")
+        .concat(data)
+    });
+  }
+  console.log(roverData);
+  return {
+    maxX,
+    maxY,
+    roverData
+  };
+};
+
 // When DOM built initialise the rendering
 document.addEventListener("DOMContentLoaded", () => {
   gridInit(grid);
+  const problemData = prepData(input);
+  renderInput(problemData.roverData);
+  renderResult(runProblem(input));
 });
